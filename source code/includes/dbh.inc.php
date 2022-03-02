93fb1d8ea3876029562037042bbd911ca0feef6f
Ryan-Chowdhary <94214471+Ryan-Chowdhary@users.noreply.github.com>
2022-03-02 15:56:16 +0530

    Add files via upload

---
+++ source code/includes/dbh.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "ryan";
+$dbPassword = "1234";
+$dbName = "chat";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+?>
